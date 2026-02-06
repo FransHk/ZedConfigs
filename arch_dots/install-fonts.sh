f65dab8f97d1a728e37b4af17417d8ac1185a3c9
frans <franshuntink@gmail.com>
2026-02-06 08:00:12 +0000

    ml2: updated font install

---
+++ arch_dots/install-fonts.sh
@@ -0,0 +1,7 @@
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/JetBrainsMono.zip
+sudo mkdir tmp_fonts
+mv JetBrainsMono.zip tmp_fonts
+cd tmp_fonts
+unzip JetBrainsMono.zip
+sudo cp *.ttf /usr/share/fonts/TTF/
+
--- arch_dots/install-packages-and-fonts.sh
+++ arch_dots/install-packages-and-fonts.sh
@@ -2,8 +2,7 @@
 
 # Update the package database
 sudo pacman -Syu
-sudo pacman -S --needed waybar wofi swaylock btop kitty swappy zed thunar firefox zed starship base-devel hyprpaper pulseaudio github-cli # basic pkgs
+sudo pacman -S --needed waybar wofi swaylock btop kitty swappy zed thunar firefox zed starship base-devel hyprpaper pulseaudio github-cli zip unzip # basic pkgs
 sudo pacman -S --needed coreutils # cutils
-sudo pacman -S --needed ttf-dejavu ttf-liberation ttf-ubuntu-font-family ttf-roboto ttf-nerd-fonts-symbols  # fonts
 
 echo "All necessary packages and fonts have been installed."
