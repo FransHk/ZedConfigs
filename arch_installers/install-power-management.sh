81d6f0524e4bf5ff7c795255aa0d7a271ce9a10d
FransHk <franshuntink@gmail.com>
2026-03-17 10:35:47 +0000

    ml2: power tools

---
+++ arch_installers/install-power-management.sh
@@ -0,0 +1,5 @@
+# Required for powerprofilesctl launching (Python GOBJECT)
+python -m pip install --upgrade pip setuptools wheel
+python -m pip install pygobject
+
+sudo pacman -S power-profiles-daemon # The actual daemon for power profiles
--- arch_installers/install-pyenv.sh
+++ arch_installers/install-pyenv.sh
@@ -1,4 +1,6 @@
 curl -fsSL https://pyenv.run | bash
 pyenv install 3.12
-pyenv global 3.12.13 # Easiest for our development in 3.12
+pyenv global 3.12.13 # Easiest for our development in 3.12, set as global
+
+# Set up direnv for easy venv loading/unloading
 sudo pacman -S direnv
