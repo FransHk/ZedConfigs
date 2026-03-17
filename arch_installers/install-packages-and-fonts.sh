393bf25c6b01fa648d4f3dacb4d207c107d1f605
FransHk <franshuntink@gmail.com>
2026-03-17 10:22:46 +0000

    zed tasks

--- arch_installers/install-packages-and-fonts.sh
+++ arch_installers/install-packages-and-fonts.sh
@@ -5,4 +5,5 @@ sudo pacman -Syu
 sudo pacman -S --needed waybar wofi swaylock btop kitty swappy zed thunar firefox zed starship base-devel hyprpaper pulseaudio github-cli zip unzip swaylock xorg-xrandr brightnessctl less hyprlock # basic pkgs 
 sudo pacman -S --needed coreutils # cutils
 
+
 echo "All necessary packages and fonts have been installed."
