838df3a1eaa45460527ce0ed2500bad215817397
FransHk <franshuntink@gmail.com>
2026-03-17 10:36:31 +0000

    finetune

--- arch_installers/install-power-management.sh
+++ arch_installers/install-power-management.sh
@@ -3,3 +3,5 @@ python -m pip install --upgrade pip setuptools wheel
 python -m pip install pygobject
 
 sudo pacman -S power-profiles-daemon # The actual daemon for power profiles
+sudo systemctl enable --now power-profiles-daemon.service
+
