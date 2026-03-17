22cd80fb6a65e98b20caee28b00d7e27b67ffeab
FransHk <franshuntink@gmail.com>
2026-03-17 10:11:25 +0000

    some instructions

--- arch_installers/install-bluetooth.sh
+++ arch_installers/install-bluetooth.sh
@@ -1,3 +1,12 @@
 sudo pacman -S bluez bluez-utils
 sudo systemctl enable --now bluetooth.service # Turn on bluetooth
 
+# Next, we use: 
+# bluetoothctl > launches bluetooth control
+# power on 
+# agent on
+# default-agent
+# scan on > will start printing MAC addresses
+# Then, use the combination of pair -> trust -> connect on the correct mac address
+
+
